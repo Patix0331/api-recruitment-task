5d5a38582c7db6ebb228156c3e50dc71ac0d2651
patix <patix0331@gmail.com>
2022-07-17 21:40:01 +0200

    feat(movieController): add randomMovie

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -4,3 +4,8 @@ import { Movie } from "../schemas/movieSchema";
 export async function addMovie(movie: Movie) {
   return repository.addMovie(movie);
 }
+
+export async function randomMovie() {
+  const movies = await repository.movies();
+  return movies[Math.floor(Math.random() * movies.length)];
+}
