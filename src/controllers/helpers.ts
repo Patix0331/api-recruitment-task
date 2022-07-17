4c12a0b1a798965a13f9a9a7e0daf0aee13cef55
patix <patix0331@gmail.com>
2022-07-17 23:23:34 +0200

    feat(helpers): add movies duplicate remover

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -3,3 +3,7 @@ import { MovieData } from "../repository/repository";
 export function random(movies: MovieData[]) {
   return movies[Math.floor(Math.random() * movies.length)];
 }
+
+export function removeDuplicates(movies: MovieData[]) {
+  return movies.filter((v, i, a) => a.findIndex(v2 => v2.title === v.title) === i)
+}
