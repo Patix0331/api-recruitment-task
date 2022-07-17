759f5f7b6eb863b27ff67fda9ee14f1e96a7841b
patix <patix0331@gmail.com>
2022-07-17 23:36:54 +0200

    refactor(movieController): move genres & duration
    Extract `genresMovies` and `durationMovies` functionality to helpers
    functions

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,4 +1,5 @@
 import { MovieData } from "../repository/repository";
+import { Genre } from "../schemas/movieSchema";
 
 export function random(movies: MovieData[]) {
   return movies[Math.floor(Math.random() * movies.length)];
@@ -6,4 +7,20 @@ export function random(movies: MovieData[]) {
 
 export function removeDuplicates(movies: MovieData[]) {
   return movies.filter((v, i, a) => a.findIndex(v2 => v2.title === v.title) === i)
+}
+
+export function durationHelper(movies: MovieData[], duration: number) {
+  const filteredMovies = movies.filter(movie => {
+    const runtime = parseInt(movie.runtime);
+    return runtime > duration - 10 && runtime < duration + 10;
+  });
+  return filteredMovies;
+}
+
+export function genresHelper(movies: MovieData[], genres: Genre[]) {
+  const uniqued = [...new Set(genres)];
+  const filteredMovies = movies.filter(movie => {
+    return uniqued.some(genre => movie.genres.includes(genre));
+  });
+  return removeDuplicates(filteredMovies);
 }
--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,6 +1,6 @@
 import { repository } from "../repository/repository";
 import { Genre, Movie } from "../schemas/movieSchema";
-import { random, removeDuplicates } from "./helpers";
+import { durationHelper, genresHelper, random, removeDuplicates } from "./helpers";
 
 export async function addMovie(movie: Movie) {
   return repository.addMovie(movie);
@@ -13,18 +13,12 @@ export async function randomMovie() {
 
 export async function durationMovies(duration: number) {
   const movies = await repository.movies();
-  const filteredMovies = movies.filter(movie => {
-    const runtime = parseInt(movie.runtime);
-    return runtime > duration - 10 && runtime < duration + 10;
-  });
+  const filteredMovies = durationHelper(movies, duration);
   return random(filteredMovies)
 }
 
 export async function genresMovies(genres: Genre[]) {
-  const uniqued = [...new Set(genres)];
   const movies = await repository.movies();
-  const filteredMovies = movies.filter(movie => {
-    return uniqued.some(genre => movie.genres.includes(genre));
-  });
-  return removeDuplicates(filteredMovies);
+  return genresHelper(movies, genres);
+}
 }
