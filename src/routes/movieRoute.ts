901362ccf6f1d897ad9b4edbbe667ac2441a5016
patix <patix0331@gmail.com>
2022-07-17 21:47:09 +0200

    feat(movieRoute): add get method which return movie
    GET method on `/movies` will return random movie from db

--- src/routes/movieRoute.ts
+++ src/routes/movieRoute.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { addMovie } from '../controllers/movieController';
+import { addMovie, randomMovie } from '../controllers/movieController';
 import { movieSchema } from '../schemas/movieSchema';
 
 export const movies = express.Router();
@@ -17,5 +17,9 @@ movies.route('/')
   .post(validateBody, async (req: Request, res: Response) => {
     const movie = await addMovie(movieSchema.parse(req.body));
     res.json(movie);
+  })
+  .get(async (req: Request, res: Response) => {
+    const movie = await randomMovie();
+    res.json(movie)
   });
 
