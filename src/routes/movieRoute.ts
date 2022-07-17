c699c43d1368f9462e88c744b1d933543233eadc
patix <patix0331@gmail.com>
2022-07-17 22:45:33 +0200

    feat(userRoute): add get method query validator

--- src/routes/movieRoute.ts
+++ src/routes/movieRoute.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { addMovie, randomMovie } from '../controllers/movieController';
 import { movieSchema } from '../schemas/movieSchema';
+import { querySchema } from '../schemas/querySchema';
 
 export const movies = express.Router();
 
@@ -13,12 +14,21 @@ function validateBody(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+function validateQuery(req: Request, res: Response, next: NextFunction) {
+  const validated = querySchema.safeParse(req.query);
+  if (!validated.success) {
+    res.status(400).json(validated.error.flatten().fieldErrors);
+  } else {
+    next();
+  }
+}
+
 movies.route('/')
   .post(validateBody, async (req: Request, res: Response) => {
     const movie = await addMovie(movieSchema.parse(req.body));
     res.json(movie);
   })
-  .get(async (req: Request, res: Response) => {
+  .get(validateQuery, async (req: Request, res: Response) => {
     const movie = await randomMovie();
     res.json(movie)
   });
