7d6c82b20e8820b45e4f4994a580875e9aa76d00
patix <patix0331@gmail.com>
2022-07-17 21:36:11 +0200

    feat(movieRoute): add movie to db on post method

--- src/routes/movieRoute.ts
+++ src/routes/movieRoute.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { addMovie } from '../controllers/movieController';
 import { movieSchema } from '../schemas/movieSchema';
 
 export const movies = express.Router();
@@ -14,6 +15,7 @@ function validateBody(req: Request, res: Response, next: NextFunction) {
 
 movies.route('/')
   .post(validateBody, async (req: Request, res: Response) => {
-    res.json(req.body);
+    const movie = await addMovie(movieSchema.parse(req.body));
+    res.json(movie);
   });
 
