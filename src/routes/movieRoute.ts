57d8a1afe1d27918afe2654386be05d6608ad37e
patix <patix0331@gmail.com>
2022-07-17 19:50:18 +0200

    feat(movieRoute): post method body validator

--- src/routes/movieRoute.ts
+++ src/routes/movieRoute.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { movieSchema } from '../schemas/movieSchema';
 
 export const movies = express.Router();
 
+function validateBody(req: Request, res: Response, next: NextFunction) {
+  const validated = movieSchema.safeParse(req.body);
+  if (!validated.success) {
+    res.status(400).json(validated.error.flatten().fieldErrors);
+  } else {
+    next();
+  }
+}
+
 movies.route('/')
-  .post(async (req: Request, res: Response) => {
-    const movie = await movieSchema.safeParseAsync(req.body);
-    if (movie.success) {
-      res.json(movie.data);
-    } else {
-      res.json(movie.error.flatten().fieldErrors);
-    }
+  .post(validateBody, async (req: Request, res: Response) => {
+    res.json(req.body);
   });
 
